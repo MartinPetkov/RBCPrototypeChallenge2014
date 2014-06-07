57c41fd643da9e04e935b41d5c221980c0e2c4f0
MartinPetkov <martin.p.petkov@gmail.com>
2014-06-07 13:33:09 -0400

    Initial models of our tables. The admin interface isn't set up yet, but I have to pull right now.

--- ATMSpotServer/ATMSpotApp/models.py
+++ ATMSpotServer/ATMSpotApp/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+class Cluster(models.Model):
+	cluster_id = models.AutoField(primary_key=True, unique=True)
+	midpoint_lat = models.IntegerField()
+	midpoint_lon = models.IntegerField()
+	score = models.IntegerField()
+
+	def __str__(self):
+		return "[midpoint_latint: (%s, %s) | Score: %s]" % (self.midpoint_lat, self.midpoint_lon, self.score)
+
+	class Admin:
+		pass
+
+
+class ATM(models.Model):
+	atm_id = models.AutoField(primary_key=True, unique=True)
+	owner = models.CharField(max_length=50)
+	address = models.CharField(max_length=50)
+	lat = models.IntegerField()
+	lon = models.IntegerField()
+	trans_per_month = models.IntegerField();
+	surcharge_type = models.CharField(max_length=10)
+	average_surchage = models.CharField(max_length=50)
+	cluster_id = models.ForeignKey(Cluster)
+
+	def __str__(self):
+		return "[Owner: %s | Address: %s | Coordinates: (%s, %s) | Montly Transactions: %s | Surcharge: %s, %s | Cluster: %s]" % (self.owner, self.address, self.lat, self.lon, self.trans_per_month, self.surcharge_type, self.average_surchage, self.cluster_id)
+
+	class Admin:
+		pass
+
+
+class Reason(models.Model):
+	reason_id = models.AutoField(primary_key=True, unique=True)
+	alignment = models.BooleanField()
+	reason_text = models.CharField(max_length=50)
+	cluster_id = models.ForeignKey(Cluster)
+
+	def __str__(self):
+		return "[Alignment: %s | Details: %s | Cluster: %s]" % (self.alignment, self.reason_text, self.cluster_id)
+
+	class Admin:
+		pass
