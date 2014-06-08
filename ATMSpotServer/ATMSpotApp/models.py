cdfc529ce34bfd98cdbd0f42dc2ba0efcc8e29ce
MartinPetkov <martin.p.petkov@gmail.com>
2014-06-07 20:00:30 -0400

    The clustering algorithm works! The main clusters GET view has some weird issue where the box can't be too big or whatever, but we'll figure it out

--- ATMSpotServer/ATMSpotApp/models.py
+++ ATMSpotServer/ATMSpotApp/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Cluster(models.Model):
 	cluster_id = models.AutoField(primary_key=True, unique=True)
-	midpoint_lat = models.IntegerField()
-	midpoint_lon = models.IntegerField()
+	midpoint_lat = models.FloatField()
+	midpoint_lon = models.FloatField()
 	score = models.IntegerField()
 
 	def __str__(self):
@@ -23,15 +23,15 @@ class ATM(models.Model):
 	atm_id = models.AutoField(primary_key=True, unique=True)
 	owner = models.CharField(max_length=50)
 	address = models.CharField(max_length=50)
-	lat = models.IntegerField()
-	lon = models.IntegerField()
+	lat = models.FloatField()
+	lon = models.FloatField()
 	trans_per_month = models.IntegerField();
 	surcharge_type = models.CharField(max_length=10, choices=SURCHARGE_TYPES)
-	average_surchage = models.CharField(max_length=50)
+	average_surcharge = models.FloatField()
 	cluster_id = models.ForeignKey(Cluster, null=True)
 
 	def __str__(self):
-		return "[Owner: %s | Address: %s | Coordinates: (%s, %s) | Montly Transactions: %s | Surcharge: %s, %s | Cluster: %s]" % (self.owner, self.address, self.lat, self.lon, self.trans_per_month, self.surcharge_type, self.average_surchage, self.cluster_id)
+		return "[Owner: %s | Address: %s | Coordinates: (%s, %s) | Montly Transactions: %s | Surcharge: %s, %f | Cluster: %s]" % (self.owner, self.address, self.lat, self.lon, self.trans_per_month, self.surcharge_type, self.average_surcharge, self.cluster_id)
 
 	class Admin:
 		pass
